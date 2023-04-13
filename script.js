32140e275ab27ada26627a3641b92f51ff28591e
Tony Rabillard <tony.rabillard16@gmail.com>
2023-04-14 00:09:55 +0200

    Add function rollDice, we can just insert img in function of random number getting

--- script.js
+++ script.js
@@ -13,3 +13,46 @@
 // tour de l’autre joueur.
 // - Lancer le dé. S’il obtient un 1, son score ROUND est perdu et c’est la fin de son tour.
 // Le premier joueur qui atteint les 100 points sur global gagne le jeu.
+let images = ['../images/face_1.png', '../images/face_2.png', '../images/face_3.png', '../images/face_4.png', '../images/face_5.png', '../images/face_6.png'];
+let currentPlayer1 = 0;
+let pictureDice = document.createElement("img");
+let divDice = document.getElementById("container_Dice");
+function getRandomNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+};
+function newGame() {
+    alert('coucou !');
+};
+
+function rollDice() {
+    let returnRandomNumber = getRandomNumber();
+    if (returnRandomNumber === 2) {
+        pictureDice.src = images[1];
+        divDice.innerHTML = '';
+        divDice.appendChild(pictureDice);
+    } else if (returnRandomNumber === 3) {
+        pictureDice.src = images[2];
+        divDice.innerHTML = '';
+        divDice.appendChild(pictureDice);
+    }
+    else if (returnRandomNumber === 4) {
+        pictureDice.src = images[3];
+        divDice.innerHTML = '';
+        divDice.appendChild(pictureDice);
+    }
+    else if (returnRandomNumber === 5) {
+        pictureDice.src = images[4];
+        divDice.innerHTML = '';
+        divDice.appendChild(pictureDice);
+    }
+    else if (returnRandomNumber === 6) {
+        pictureDice.src = images[5];
+        divDice.innerHTML = '';
+        divDice.appendChild(pictureDice);
+    }
+    else {
+        pictureDice.src = images[0];
+        divDice.innerHTML = '';
+        divDice.appendChild(pictureDice);
+    }
+}
